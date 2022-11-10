1800f8929c2a87687acd74301d9fe7b65e7f39e5
bharathrico <bharathganesh26899@gmail.com>
2022-11-10 18:35:18 +0530

    newadditions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <svg className='circle1'  width="100" height="100">
+        <circle  cx="50" cy="50" r="20" stroke="#FFAD38" stroke-width="0.1"  fillOpacity='0'/>
+        <circle  cx="64" cy="64" r="0.4" stroke="#FFAD38" stroke-width="1"  fill="#FFAD38"/>
+      </svg>
+      <div className='text'>Portfolio</div>
     </div>
   );
 }
