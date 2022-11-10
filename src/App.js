d841e97c277777d384f03e941404ca9d9bb91612
bharathrico <bharathganesh26899@gmail.com>
2022-11-10 20:38:19 +0530

    newchanges

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './styles/App.css';
+import {useRef,useState} from 'react'
 
 function App() {
+  
+  const [circlearray,setArray]=useState([130,252,50.3])
+
+  const maincircle = useRef(null)
+
   return (
-    <div className="App">
-      <svg className='circle1'  width="100" height="100">
-        <circle  cx="50" cy="50" r="20" stroke="#FFAD38" stroke-width="0.1"  fillOpacity='0'/>
-        <circle  cx="64" cy="64" r="0.4" stroke="#FFAD38" stroke-width="1"  fill="#FFAD38"/>
+    <div className="App"> 
+      <svg className='circle1' width="1" height="1">
+        <circle className='og-circle' ref={maincircle}  cx="0" cy="0" r="20" stroke="#FFAD38" strokeLinecap='round' strokeWidth="0.5" strokeDashoffset={circlearray[0]} strokeDasharray={circlearray[1]} transform={"rotate("+circlearray[2]+")"} fillOpacity='0'/>
+        <circle className='og-circle2' cx="14.2" cy="14.2" r="0.4" stroke="#FFAD38" strokeWidth="1" onMouseOver={(event)=>{event.target.setAttribute('r', '3');setArray([145,260,60])}} onMouseLeave={(event)=>{event.target.setAttribute('r', '0.4');setArray([130,252,50.3])}}  fill="#FFAD38"/>
       </svg>
       <div className='text'>Portfolio</div>
     </div>
